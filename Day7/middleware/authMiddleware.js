3d00d758cb5f96c262064bb554178e2ef0482984
Zeba <zeba.roohi26@gmail.com>
2024-11-07 19:03:43 +0530

    complete jwt and backend of eventmanagement

--- Day7/middleware/authMiddleware.js
+++ Day7/middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt=require('jsonwebtoken')
 
 const authMiddleware=(req,res,next)=>{
-    const token=req.header['authorization']
+    const token=req.headers['authorization'].split(' ')[1];
     if(!token) return res.status(403).json({message:'Token is required'})
 
     jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
